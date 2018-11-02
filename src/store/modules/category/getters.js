993c73dc994d020218afc51a0a40403113bc5c5c
cunyang.liu <cunyang.liu@zhangmen.com>
2018-11-02 14:18:31 +0800

    完成分类页面（楼梯效果），添加防抖事件

--- src/store/modules/category/getters.js
+++ src/store/modules/category/getters.js
@@ -3,15 +3,14 @@ export default {
         let _arr = [];
         state.categoryList.map(function(item) { 
 
-
-
-            let _list_fill = item.category_list.filter(function(item_item){
+            const _list_fill = item.category_list.filter(function(item_item){
                 return item_item.view_type == 'cells_auto_fill';
             });
 
-            let _list_group = item.category_list.filter(function(item_item){
+            const _list_group = item.category_list.filter(function(item_item){
                 return item_item.view_type == 'category_group';
             })[0].body.items;
+
             let _list_fill_img = '';
             if(_list_fill.length>0) _list_fill_img = _list_fill[0].body.items[0].img_url;
             
--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,24 +1,21 @@
 <template>
 <div class="category">
     <category-head></category-head>
-    <category-btn></category-btn>
     <category-main></category-main>
 	<foot-tab></foot-tab> 
 </div>
 </template>
 
 <script>
-import { categoryData } from '@/server/category.js';
-import { mapActions } from 'vuex';
+// import { categoryData } from '@/server/category.js';
+// import { mapActions } from 'vuex';
 import categoryHead from '@/components/header/category.vue';
-import footTab from '@/components/footer/index.vue';
-import categoryBtn from './components/categoryBtn.vue';
+import footTab from '@/components/footer/index.vue'; 
 import categoryMain from './components/categoryMain.vue';
 export default {
     components:{
         categoryHead,
-        footTab,
-        categoryBtn,
+        footTab, 
         categoryMain
     },
     data(){
@@ -27,18 +24,16 @@ export default {
         }
     },
     async mounted(){
-        const _data = await categoryData();
-        this.getCategoryList(_data.data);
+        // const _data = await categoryData();
+        // this.getCategoryList(_data.data);
     },
     methods:{
-        ...mapActions('category',[
-            'getCategoryList'
-        ])
+        // ...mapActions('category',[
+        //     'getCategoryList'
+        // ])
     }
 }; 
 </script>
 
 <style lang="less" scoped>
-.category{ 
-}
 </style>
